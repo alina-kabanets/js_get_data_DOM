f956c3b6713851157cf72c5b22efb904eafc61fe
Alina Kabanets <kabanets.dolls@gmail.com>
2023-12-05 11:44:27 +0000

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const data = document.querySelectorAll(".population");
+const numbers = [
+    ...data
+].map((bit)=>+bit.textContent.split(",").join(""));
+const total = numbers.reduce((acc, current)=>acc + current, 0);
+const average = total / numbers.length;
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+totalPopulation.textContent = total.toLocaleString("en-US");
+averagePopulation.textContent = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
